aee678a5771c4d26817a5bf9ecdf12e532fa8e8c
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-03-05 20:21:10 -0500

    Working conference call demo

---
+++ webrtc/broadcast-server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"sync"
+
+	"fmt"
+
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+type Broadcaster struct {
+	sync.RWMutex
+	clients []*websocket.Conn
+}
+
+func (b *Broadcaster) Handler(conn *websocket.Conn) {
+	b.Lock()
+	b.clients = append(b.clients, conn)
+	b.Unlock()
+
+	var message interface{}
+	for {
+		err := websocket.JSON.Receive(conn, &message)
+		if err != nil {
+			fmt.Printf("Failed to read from websocket: %s\n", err.Error())
+
+			b.Lock()
+			for i, c := range b.clients {
+				if c == conn {
+					b.clients[i] = b.clients[len(b.clients)-1]
+					b.clients = b.clients[:len(b.clients)-1]
+					break
+				}
+			}
+			b.Unlock()
+			return
+		}
+
+		b.RLock()
+		for _, c := range b.clients {
+			websocket.JSON.Send(c, message)
+		}
+		b.RUnlock()
+	}
+}
+
+func main() {
+	b := Broadcaster{}
+	http.Handle("/ws", websocket.Handler(b.Handler))
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":3434", nil)
+}
